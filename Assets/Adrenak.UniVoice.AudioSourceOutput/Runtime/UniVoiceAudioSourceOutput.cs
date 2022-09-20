ab8ad90c38231e97fb4198fa72ee701515b3c313
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2022-09-20 22:48:53 +0530

    Update to UniVoice 2.0.0 Rename InbuiltAudioBuffer to CircularAudioClip skip ci

--- Assets/Adrenak.UniVoice.AudioSourceOutput/Runtime/UniVoiceAudioSourceOutput.cs
+++ Assets/Adrenak.UniVoice.AudioSourceOutput/Runtime/UniVoiceAudioSourceOutput.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using UnityEngine;
 
 namespace Adrenak.UniVoice.AudioSourceOutput {
-    // ========================================================================
-    #region OUTPUT IMPLEMENTATION
-    // ========================================================================
     /// <summary>
     /// This class feeds incoming segments of audio to an AudioBuffer 
     /// and plays the buffer's clip on an AudioSource. It also clears segments
@@ -26,20 +23,21 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
             return matches.Count();
         }
 
-        public InbuiltAudioBuffer AudioBuffer { get; private set; }
         public AudioSource AudioSource { get; private set; }
         public int MinSegCount { get; private set; }
 
-        // Prevent construction using 'new' as this is a MonoBehaviour class
-        UniVoiceAudioSourceOutput() { }
+        CircularAudioClip circularAudioClip;
 
         public string ID {
-            get => AudioBuffer.AudioClip.name;
+            get => circularAudioClip.AudioClip.name;
             set {
                 gameObject.name = "UniVoice Peer #" + value;
-                AudioBuffer.AudioClip.name = "UniVoice Peer #" + value;
+                circularAudioClip.AudioClip.name = "UniVoice Peer #" + value;
             }
         }
+
+        [System.Obsolete("Cannot use new keyword to create an instance. Use .New() method instead")]
+        public UniVoiceAudioSourceOutput() { }
 
         /// <summary>
         /// Creates a new instance using the dependencies.
@@ -54,14 +52,14 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
         /// </param>
         /// 
         /// <param name="minSegCount">
-        /// The minimum number of audio segments <see cref="AudioBuffer"/> 
+        /// The minimum number of audio segments <see cref="circularAudioClip"/> 
         /// must have for the streamer to play the audio. This value is capped
-        /// between 1 and <see cref="InbuiltAudioBuffer.SegCount"/> of the 
-        /// <see cref="AudioBuffer"/> passed.
+        /// between 1 and <see cref="CircularAudioClip.SegCount"/> of the 
+        /// <see cref="circularAudioClip"/> passed.
         /// Default: 0. Results in the value being set to the max possible.
         /// </param>
         public static UniVoiceAudioSourceOutput New
-        (InbuiltAudioBuffer buffer, AudioSource source, int minSegCount = 0) {
+        (CircularAudioClip buffer, AudioSource source, int minSegCount = 0) {
             var ctd = source.gameObject.AddComponent<UniVoiceAudioSourceOutput>();
             DontDestroyOnLoad(ctd.gameObject);
 
@@ -72,7 +70,7 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
                 ctd.MinSegCount = Mathf.Clamp(minSegCount, 1, buffer.SegCount);
             else
                 ctd.MinSegCount = buffer.SegCount;
-            ctd.AudioBuffer = buffer;
+            ctd.circularAudioClip = buffer;
             ctd.AudioSource = source;
 
             return ctd;
@@ -80,21 +78,20 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
 
         int lastIndex = -1;
         /// <summary>
-        /// (silence). 
         /// This is to make sure that if a segment is missed, its previous 
         /// contents won't be played again when the clip loops back.
         /// </summary>
         private void Update() {
             if (AudioSource.clip == null) return;
 
-            var index = (int)(AudioSource.Position() * AudioBuffer.SegCount);
+            var index = (int)(AudioSource.Position() * circularAudioClip.SegCount);
 
             // Check every frame to see if the AudioSource has 
             // just moved to a new segment in the AudioBuffer 
             if (lastIndex != index) {
                 // If so, clear the audio buffer so that in case the
                 // AudioSource loops around, the old contents are not played.
-                AudioBuffer.Clear(lastIndex);
+                circularAudioClip.Clear(lastIndex);
 
                 segments.EnsureKey(lastIndex, Status.Behind);
                 segments.EnsureKey(index, Status.Current);
@@ -131,17 +128,17 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
             // It's been passed already without playing.
             if (segments.ContainsKey(index)) return;
 
-            int locIdx = (int)(AudioSource.Position() * AudioBuffer.SegCount);
-            locIdx = Mathf.Clamp(locIdx, 0, AudioBuffer.SegCount - 1);
+            int locIdx = (int)(AudioSource.Position() * circularAudioClip.SegCount);
+            locIdx = Mathf.Clamp(locIdx, 0, circularAudioClip.SegCount - 1);
 
-            var bufferIndex = AudioBuffer.GetNormalizedIndex(index);
+            var bufferIndex = circularAudioClip.GetNormalizedIndex(index);
 
             // Don't write to the same segment index that we are reading
             if (locIdx == bufferIndex) return;
 
             // Finally write into the buffer 
             segments.Add(index, Status.Ahead);
-            AudioBuffer.Write(index, audioSamples);
+            circularAudioClip.Write(index, audioSamples);
         }
 
         /// <summary>
@@ -150,139 +147,30 @@ namespace Adrenak.UniVoice.AudioSourceOutput {
         public void Dispose() {
             Destroy(gameObject);
         }
-    }
-    #endregion
-
-    // ========================================================================
-    #region OUTPUT FACTORY IMPLEMENTATION
-    // ========================================================================
-    /// <summary>
-    /// Creates <see cref="UniVoiceAudioSourceOutput"/> instances
-    /// </summary>
-    public class InbuiltAudioOutputFactory : IAudioOutputFactory {
-        public int BufferSegCount { get; private set; }
-        public int MinSegCount { get; private set; }
-
-        public InbuiltAudioOutputFactory() : this(10, 5) { }
-
-        public InbuiltAudioOutputFactory(
-        int bufferSegCount, int minSegCount) {
-            BufferSegCount = bufferSegCount;
-            MinSegCount = minSegCount;
-        }
-
-        public IAudioOutput Create
-        (int samplingRate, int channelCount, int segmentLength) {
-            return UniVoiceAudioSourceOutput.New(
-                new InbuiltAudioBuffer(
-                    samplingRate, channelCount, segmentLength, BufferSegCount
-                ),
-                new GameObject($"UniVoice Peer").AddComponent<AudioSource>(),
-                MinSegCount
-            );
-        }
-    }
-    #endregion
-
-    // ========================================================================
-    #region INTERNAL BUFFER
-    // ========================================================================
-    /// <summary>
-    /// Used to arrange irregular, out of order 
-    /// and skipped audio segments for better playback.
-    /// </summary>
-    public class InbuiltAudioBuffer {
-        public AudioClip AudioClip { get; private set; }
-        public int SegCount { get; private set; }
-        public int SegDataLen { get; private set; }
-
-        // Holds the first valid segment index received by the buffer
-        int firstIndex;
 
         /// <summary>
-        /// Create an instance
+        /// Creates <see cref="UniVoiceAudioSourceOutput"/> instances
         /// </summary>
-        /// <param name="frequency">The frequency of the audio</param>
-        /// <param name="channels">Number of channels in the audio</param>
-        /// <param name="segDataLen">Number of samples in the audio </param>
-        /// <param name="segCount">Number of segments stored in buffer </param>
-        public InbuiltAudioBuffer(
-            int frequency,
-            int channels,
-            int segDataLen,
-            int segCount = 3,
-            string clipName = null
-        ) {
-            clipName = clipName ?? "clip";
-            AudioClip = AudioClip.Create(
-                clipName,
-                segDataLen * segCount,
-                channels,
-                frequency,
-                false
-            );
+        public class Factory : IAudioOutputFactory {
+            public int BufferSegCount { get; private set; }
+            public int MinSegCount { get; private set; }
 
-            firstIndex = -1;
-            SegDataLen = segDataLen;
-            SegCount = segCount;
+            public Factory() : this(10, 5) { }
+
+            public Factory(int bufferSegCount, int minSegCount) {
+                BufferSegCount = bufferSegCount;
+                MinSegCount = minSegCount;
+            }
+
+            public IAudioOutput Create(int samplingRate, int channelCount, int segmentLength) {
+                return New(
+                    new CircularAudioClip(
+                        samplingRate, channelCount, segmentLength, BufferSegCount
+                    ),
+                    new GameObject($"UniVoiceAudioSourceOutput").AddComponent<AudioSource>(),
+                    MinSegCount
+                );
+            }
         }
-
-        /// <summary>
-        /// Feed an audio segment to the buffer.
-        /// </summary>
-        /// 
-        /// <param name="absoluteIndex">
-        /// Absolute index of the audio segment from the source.
-        /// </param>
-        /// 
-        /// <param name="audioSegment">Audio samples data</param>
-        public bool Write(int absoluteIndex, float[] audioSegment) {
-            // Reject if the segment length is wrong
-            if (audioSegment.Length != SegDataLen) return false;
-
-            if (absoluteIndex < 0 || absoluteIndex < firstIndex) return false;
-
-            // If this is the first segment fed
-            if (firstIndex == -1) firstIndex = absoluteIndex;
-
-            // Convert the absolute index into a looped-around index
-            var localIndex = GetNormalizedIndex(absoluteIndex);
-
-            // Set the segment at the clip data at the right index
-            if (localIndex >= 0)
-                AudioClip.SetData(audioSegment, localIndex * SegDataLen);
-            return true;
-        }
-
-        /// <summary>
-        /// Returns the index after looping around the buffer
-        /// </summary>
-        public int GetNormalizedIndex(int absoluteIndex) {
-            if (firstIndex == -1 || absoluteIndex <= firstIndex) return -1;
-            return (absoluteIndex - firstIndex) % SegCount;
-        }
-
-        /// <summary>
-        /// Clears the buffer at the specified local index
-        /// </summary>
-        /// <param name="index"></param>
-        public bool Clear(int index) {
-            if (index < 0) return false;
-
-            // If the index is out of bounds, then we
-            // loop that around and use the local index
-            if (index >= SegCount)
-                index = GetNormalizedIndex(index);
-            AudioClip.SetData(new float[SegDataLen], index * SegDataLen);
-            return true;
-        }
-
-        /// <summary>
-        /// Clear the entire buffer
-        /// </summary>
-        public void Clear() {
-            AudioClip.SetData(new float[SegDataLen * SegCount], 0);
-        }
-        #endregion
     }
 }
